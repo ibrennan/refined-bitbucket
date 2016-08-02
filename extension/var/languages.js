a41914e985803167f46018ff2b03f502813eec8c
Ian Brennan <ian@ibrennan.co.uk>
2016-08-02 16:08:31 +0100

    Updated languages to support TypeScript

--- extension/var/languages.js
+++ extension/var/languages.js
@@ -37,5 +37,6 @@ define({
     ".scala": "language-scala",
     ".swift": "language-swift",
     ".yaml": "language-yaml",
-    ".yml": "language-yaml"
+    ".yml": "language-yaml",
+    ".ts": "language-typescript"
 });
